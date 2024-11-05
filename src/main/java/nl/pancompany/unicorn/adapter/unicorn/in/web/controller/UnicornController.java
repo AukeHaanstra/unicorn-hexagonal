9f80ecc72fc309efa82703bdbed3a07acc17289f
Auke Haanstra <aukehaanstra@gmail.com>
2024-11-06 00:36:59 +0100

    Small fix

--- src/main/java/nl/pancompany/unicorn/adapter/unicorn/in/web/controller/UnicornController.java
+++ src/main/java/nl/pancompany/unicorn/adapter/unicorn/in/web/controller/UnicornController.java
@@ -22,7 +22,7 @@ public class UnicornController {
     private final UnicornViewMapper unicornViewMapper;
 
     @GetMapping
-    public ResponseEntity<UnicornView> getLeg(@PathVariable("unicornId") String unicornId) {
+    public ResponseEntity<UnicornView> getUnicorn(@PathVariable("unicornId") String unicornId) {
         Unicorn.UnicornDto unicorn = getUnicornUsecase.getUnicorn(Unicorn.UnicornId.of(unicornId));
         return ResponseEntity.ok(unicornViewMapper.map(unicorn));
     }
